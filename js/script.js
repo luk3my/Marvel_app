718d35db0f3d645c585719146be6b956cac8b36b
luk3my <luk3myoung@gmail.com>
2018-01-20 09:03:29 +1000

    changes
    css changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-  var PRIV_KEY = "";
-  var PUBLIC_KEY = "";
+  var PRIV_KEY = "40828e009f670abd1e617a3fb95e0418a898b757";
+  var PUBLIC_KEY = "bf41cf76b8b7ccb4bc29c795ec5caa8c";
 
   function getMarvelResponse() {
 
@@ -27,22 +27,25 @@ $(document).ready(function(){
 
         var results = response.data.results;
         var resultsLen = results.length;
-        var output = '<ul>'
+        var output = '<ul id="comics">'
 
        for( var i=0; i<resultsLen; i++){
           if(results[i].images.length > 0) {
             var imgPath = results[i].images[0].path + '/standard_xlarge.' +
             results[i].images[0].extension;
-            output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
+            output += '<li><img src="' + imgPath + '"><br><label>'+results[i].title+'</label></li>';
 
           }
         }
 
-        output += '<ul>'
+        output += '</ul>'
         $('#results').append(output);
 
 
         console.log(imgPath);
+
+
+
 
 })
 
@@ -54,3 +57,20 @@ $(document).ready(function(){
 
   getMarvelResponse();
 });
+
+function searchComicTitles() {
+  var input = document.getElementById('searchbar')
+  var filter = input.value.toUpperCase();
+  var ul = document.getElementById('comics');
+  var text = ul.getElementsByTagName('li');
+
+  for (var i=0; i< text.length; i++){
+        var newComic = text[i].getElementsByTagName('label')[0];
+        if (newComic.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          text[i].style.display = "";
+        }else{
+          text[i].style.display = "none";
+        }
+    }
+
+};
